7333072570dc6dae708d43bb7a9fc561549eb538
Jana Jolovic <janajolovic04@gmail.com>
2022-01-14 19:43:52 +0100

    izbrisan meni za kategorije...

--- app/src/main/java/com/example/oslobodiseresi/Activity/MainActivity.java
+++ app/src/main/java/com/example/oslobodiseresi/Activity/MainActivity.java
@@ -55,94 +55,94 @@ public class MainActivity extends ToolbarNavigacijaSetup {
         navigationView.setNavigationItemSelectedListener(this);
 
         setToolbar(true);
-
-        recyclerArtikli.setLayoutManager(new GridLayoutManager(this, 2));
-
-        spinnerKategorije = findViewById(R.id.spinnerKategorije);
-        spinnerGradovi = findViewById(R.id.spinnerGradovi);
-
-            MutableLiveData<ArrayList<Kategorija>> mldKategorije = ItemRepository.getInstance(MainApplication.apiManager).getAllKategorije();
-            mldKategorije.observe(MainActivity.this, new Observer<ArrayList<Kategorija>>() {
-                @Override
-                public void onChanged(ArrayList<Kategorija> kategorija) {
-                        ArrayList<String> kategorije = new ArrayList<>();
-                        kategorije.add("Sve kategorije");
-                        for (Kategorija k : mldKategorije.getValue()) {
-                            kategorije.add(k.getNaziv());
-                        }
-                        ArrayAdapter<String> kategorijeAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, kategorije);
-                        kategorijeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                        spinnerKategorije.setAdapter(kategorijeAdapter);
-                    }
-            });
-
-
-            MutableLiveData<ArrayList<Grad>> mldGradovi = ItemRepository.getInstance(MainApplication.apiManager).getAllGradovi();
-            mldGradovi.observe(MainActivity.this, new Observer<ArrayList<Grad>>() {
-                @Override
-                public void onChanged(ArrayList<Grad> grad) {
-                    ArrayList<String> gradovi = new ArrayList<>();
-                    gradovi.add("Svi gradovi");
-                    for (Grad g : mldGradovi.getValue()) {
-                            gradovi.add(g.getNaziv());
-                        }
-                        ArrayAdapter<String> gradoviAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, gradovi);
-                        gradoviAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                        spinnerGradovi.setAdapter(gradoviAdapter);
-                        //spinnerGradovi.setOnItemSelectedListener(MainActivity.this);
-                    }
-            });
-
-        //recycler view
-        ArtikalAdapter adapterArtikli = new ArtikalAdapter(this);
-
-        MutableLiveData<ArrayList<Item>> artikli = UserRepository.getInstance(MainApplication.apiManager).GetAllItems();
-        artikli.observe(MainActivity.this, new Observer<ArrayList<Item>>() {
-            @Override
-            public void onChanged(ArrayList<Item> items) {
-                adapterArtikli.setArtikli(artikli.getValue());
-                recyclerArtikli.setAdapter(adapterArtikli);
-                progress.setVisibility(View.INVISIBLE);
-            }
-        });
-
-        SearchView searchView = findViewById(R.id.search_bar);
-        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                adapterArtikli.getFilter().filter(newText);
-                return false;
-            }
-        });
-
-        primeniFiltere.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MutableLiveData<ArrayList<Item>> mld;
-                if (spinnerGradovi.getSelectedItemPosition() != 0 && spinnerKategorije.getSelectedItemPosition() != 0) {
-                    mld = ItemRepository.getInstance(MainApplication.apiManager).getItemFromKategorijaGrad(
-                            spinnerKategorije.getSelectedItemPosition(),
-                            spinnerGradovi.getSelectedItemPosition()
-                    );
-                }else if(spinnerKategorije.getSelectedItemPosition() == 0 && spinnerGradovi.getSelectedItemPosition() == 0){
-                    mld = ItemRepository.getInstance(MainApplication.apiManager).getAllItems();
-                }else if(spinnerKategorije.getSelectedItemPosition() != 0){
-                    mld = ItemRepository.getInstance(MainApplication.apiManager).getItemsFromKategorija(spinnerKategorije.getSelectedItemPosition());
-                }else{
-                    mld = ItemRepository.getInstance(MainApplication.apiManager).getItemsFromGrad(spinnerGradovi.getSelectedItemPosition());
-                }
-                mld.observe(MainActivity.this, new Observer<ArrayList<Item>>() {
-                    @Override
-                    public void onChanged(ArrayList<Item> items) {
-                        adapterArtikli.setArtikli(mld.getValue());
-                    }
-                });
-            }
-        });
+//
+//        recyclerArtikli.setLayoutManager(new GridLayoutManager(this, 2));
+//
+//        spinnerKategorije = findViewById(R.id.spinnerKategorije);
+//        spinnerGradovi = findViewById(R.id.spinnerGradovi);
+//
+//            MutableLiveData<ArrayList<Kategorija>> mldKategorije = ItemRepository.getInstance(MainApplication.apiManager).getAllKategorije();
+//            mldKategorije.observe(MainActivity.this, new Observer<ArrayList<Kategorija>>() {
+//                @Override
+//                public void onChanged(ArrayList<Kategorija> kategorija) {
+//                        ArrayList<String> kategorije = new ArrayList<>();
+//                        kategorije.add("Sve kategorije");
+//                        for (Kategorija k : mldKategorije.getValue()) {
+//                            kategorije.add(k.getNaziv());
+//                        }
+//                        ArrayAdapter<String> kategorijeAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, kategorije);
+//                        kategorijeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//                        spinnerKategorije.setAdapter(kategorijeAdapter);
+//                    }
+//            });
+//
+//
+//            MutableLiveData<ArrayList<Grad>> mldGradovi = ItemRepository.getInstance(MainApplication.apiManager).getAllGradovi();
+//            mldGradovi.observe(MainActivity.this, new Observer<ArrayList<Grad>>() {
+//                @Override
+//                public void onChanged(ArrayList<Grad> grad) {
+//                    ArrayList<String> gradovi = new ArrayList<>();
+//                    gradovi.add("Svi gradovi");
+//                    for (Grad g : mldGradovi.getValue()) {
+//                            gradovi.add(g.getNaziv());
+//                        }
+//                        ArrayAdapter<String> gradoviAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, gradovi);
+//                        gradoviAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//                        spinnerGradovi.setAdapter(gradoviAdapter);
+//                        //spinnerGradovi.setOnItemSelectedListener(MainActivity.this);
+//                    }
+//            });
+//
+//        //recycler view
+//        ArtikalAdapter adapterArtikli = new ArtikalAdapter(this);
+//
+//        MutableLiveData<ArrayList<Item>> artikli = UserRepository.getInstance(MainApplication.apiManager).GetAllItems();
+//        artikli.observe(MainActivity.this, new Observer<ArrayList<Item>>() {
+//            @Override
+//            public void onChanged(ArrayList<Item> items) {
+//                adapterArtikli.setArtikli(artikli.getValue());
+//                recyclerArtikli.setAdapter(adapterArtikli);
+//                progress.setVisibility(View.INVISIBLE);
+//            }
+//        });
+//
+//        SearchView searchView = findViewById(R.id.search_bar);
+//        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                return false;
+//            }
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                adapterArtikli.getFilter().filter(newText);
+//                return false;
+//            }
+//        });
+//
+//        primeniFiltere.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                MutableLiveData<ArrayList<Item>> mld;
+//                if (spinnerGradovi.getSelectedItemPosition() != 0 && spinnerKategorije.getSelectedItemPosition() != 0) {
+//                    mld = ItemRepository.getInstance(MainApplication.apiManager).getItemFromKategorijaGrad(
+//                            spinnerKategorije.getSelectedItemPosition(),
+//                            spinnerGradovi.getSelectedItemPosition()
+//                    );
+//                }else if(spinnerKategorije.getSelectedItemPosition() == 0 && spinnerGradovi.getSelectedItemPosition() == 0){
+//                    mld = ItemRepository.getInstance(MainApplication.apiManager).getAllItems();
+//                }else if(spinnerKategorije.getSelectedItemPosition() != 0){
+//                    mld = ItemRepository.getInstance(MainApplication.apiManager).getItemsFromKategorija(spinnerKategorije.getSelectedItemPosition());
+//                }else{
+//                    mld = ItemRepository.getInstance(MainApplication.apiManager).getItemsFromGrad(spinnerGradovi.getSelectedItemPosition());
+//                }
+//                mld.observe(MainActivity.this, new Observer<ArrayList<Item>>() {
+//                    @Override
+//                    public void onChanged(ArrayList<Item> items) {
+//                        adapterArtikli.setArtikli(mld.getValue());
+//                    }
+//                });
+//            }
+//        });
     }
 }
