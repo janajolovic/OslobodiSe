abfd0496e4ce25868bf4e25f685bb1ce70a9eb33
DinoZ1729 <dinozunic2004@gmail.com>
2022-01-26 22:53:00 +0100

    #43

--- app/src/main/java/com/example/oslobodiseresi/KomentarAdapter.java
+++ app/src/main/java/com/example/oslobodiseresi/KomentarAdapter.java
@@ -22,6 +22,7 @@ import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.oslobodiseresi.Activity.ArtikalActivity;
+import com.example.oslobodiseresi.Activity.MainActivity;
 import com.example.oslobodiseresi.Activity.MojProfilActivity;
 import com.example.oslobodiseresi.Activity.ProfilKorisnika;
 import com.example.oslobodiseresi.Models.Item;
@@ -58,42 +59,14 @@ public class KomentarAdapter extends RecyclerView.Adapter<KomentarAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
-        final boolean[] lajkovan = {komentari.get(position).isLajkovan()};
-        if(lajkovan[0]) {
-            holder.imgLajk.setImageResource(R.drawable.ic_baseline_thumb_up_24);
-        } else {
-            holder.imgLajk.setImageResource(R.drawable.ic_outline_thumb_up_24);
-        }
-
-        holder.imgLajk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(Utils.getInstance().jeUlogovan()){
-                    UserRepository.getInstance(MainApplication.apiManager).LajkujKomentar(komentari.get(position).getId(), Utils.getInstance().getKorisnik().getId());
-                    lajkovan[0] = !lajkovan[0];
-                    if(lajkovan[0]){
-                        holder.imgLajk.setImageResource(R.drawable.ic_baseline_thumb_up_24);
-                        holder.txtBrojGlasova.setText(String.valueOf(Integer.parseInt(holder.txtBrojGlasova.getText().toString())+1));
-                        Utils.getInstance().getKorisnik().getLajkovaniKomentari().add(komentari.get(position).getId());
-                    } else {
-                        holder.imgLajk.setImageResource(R.drawable.ic_outline_thumb_up_24);
-                        holder.txtBrojGlasova.setText(String.valueOf(Integer.parseInt(holder.txtBrojGlasova.getText().toString())-1));
-                        Utils.getInstance().getKorisnik().getLajkovaniKomentari().remove(Integer.valueOf(komentari.get(position).getId()));
-                    }
-                    Utils.getInstance().SacuvajKorisnika(Utils.getInstance().getKorisnik());
-                } else {
-                    Toast.makeText(context, "Morate biti prijavljeni da biste lajkovali komentare", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        holder.txtIme.setText(komentari.get(position).getKorisnik().getIme());
+        holder.txtSadrzaj.setText(komentari.get(position).getSadrzaj());
 
         holder.txtIme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(Utils.getInstance().jeUlogovan() && Utils.getInstance().getKorisnik().getId().equals(komentari.get(position).getKorisnik().getId())){
-                    Intent intent = new Intent(context, MojProfilActivity.class);
-                    context.startActivity(intent);
+                    context.startActivity(new Intent(context, MojProfilActivity.class));
                 } else {
                     Intent intent = new Intent(context, ProfilKorisnika.class);
                     intent.putExtra("KORISNIK_ID", komentari.get(position).getKorisnik().getId());
@@ -106,8 +79,7 @@ public class KomentarAdapter extends RecyclerView.Adapter<KomentarAdapter.ViewHo
             @Override
             public void onClick(View v) {
                 if(Utils.getInstance().jeUlogovan() && Utils.getInstance().getKorisnik().getId().equals(komentari.get(position).getKorisnik().getId())){
-                    Intent intent = new Intent(context, MojProfilActivity.class);
-                    context.startActivity(intent);
+                    context.startActivity(new Intent(context, MojProfilActivity.class));
                 } else {
                     Intent intent = new Intent(context, ProfilKorisnika.class);
                     intent.putExtra("KORISNIK_ID", komentari.get(position).getKorisnik().getId());
@@ -115,50 +87,6 @@ public class KomentarAdapter extends RecyclerView.Adapter<KomentarAdapter.ViewHo
                 }
             }
         });
-
-        MutableLiveData<ResponseBody> mld = UserRepository.getInstance(MainApplication.apiManager).GetProfilna(komentari.get(position).getKorisnik().getId());
-        mld.observe((AppCompatActivity)context, new Observer<ResponseBody>() {
-            @Override
-            public void onChanged(ResponseBody responseBody) {
-                Bitmap bmp = BitmapFactory.decodeStream(responseBody.byteStream());
-                holder.imgProfil.setImageBitmap(bmp);
-                Log.println(Log.ASSERT,komentari.get(position).getSadrzaj().substring(0,10)+"...", bmp.toString().substring(0,20));
-            }
-        });
-
-        holder.txtBrojGlasova.setText(String.valueOf(komentari.get(position).getBrojLajkova()));
-
-        holder.txtSadrzaj.setText(komentari.get(position).getSadrzaj());
-
-        holder.izbrisi.setVisibility(View.INVISIBLE);
-
-        if(Utils.getInstance().jeUlogovan() && Utils.getInstance().getKorisnik().getId().equals(komentari.get(position).getKorisnik().getId())) {
-            holder.izbrisi.setVisibility(View.VISIBLE);
-            holder.izbrisi.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                    builder.setMessage("Da li ste sigurni da zelite da obrisete ovaj komentar?");
-                    builder.setPositiveButton("Da", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ItemRepository.getInstance(MainApplication.apiManager).IzbrisiKomentar(komentari.get(position).getId());
-                            komentari.remove(position);
-                            notifyItemRemoved(position);
-                        }
-                    });
-                    builder.setNegativeButton("Ne", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                        }
-                    });
-                    builder.show();
-                }
-            });
-        }
-
-        holder.txtIme.setText(komentari.get(position).getKorisnik().getIme());
-
     }
 
     @Override
