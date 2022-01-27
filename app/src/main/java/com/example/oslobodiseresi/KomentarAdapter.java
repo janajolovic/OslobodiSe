2583b3d5ebf61c03adb4baa371407f4e72a95a3a
DinoZ1729 <dinozunic2004@gmail.com>
2022-01-27 01:27:09 +0100

    #57

--- app/src/main/java/com/example/oslobodiseresi/KomentarAdapter.java
+++ app/src/main/java/com/example/oslobodiseresi/KomentarAdapter.java
@@ -103,7 +103,7 @@ public class KomentarAdapter extends RecyclerView.Adapter<KomentarAdapter.ViewHo
                             mld.observe((AppCompatActivity)context, new Observer<String>() {
                                 @Override
                                 public void onChanged(String s) {
-                                    komentari.remove(Integer.valueOf(holder.getAdapterPosition()));
+                                    komentari.remove(Integer.valueOf(komentari.get(holder.getAdapterPosition()).getId()));
                                     notifyItemRemoved(holder.getAdapterPosition());
                                     notifyItemRangeChanged(holder.getAdapterPosition(), getItemCount());
                                 }
